9cd36b2d4c6e670ab9a4d48b30fd689932ed5ed6
princekin116 <princekin.nicholas@gmail.com>
2023-09-17 15:06:31 +0800

    新增PlayerController
    新增Wizard Gril 资源包
    新增CameraFollow功能

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private CharacterController _character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _character = GetComponent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(horizontal,0,vertical);
+
+        if (dir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(dir);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        }
+        
+    }
+}
---
+++ Assets/ThirdParty/ReadMe.md
@@ -0,0 +1,5 @@
+插件 统一放在ThirParty目录下
+- 按插件名建立分类文件夹
+
+
+详细分类请查看项目根目录下Documents/Development/ReadMe.md
