2e9888b5d37a88b6b0ab62f6335af892c7c2e2e8
princekin116 <princekin.nicholas@gmail.com>
2023-09-17 20:14:08 +0800

    新增人物动画Flip，奔跑
    新增Kawaii史莱姆资源包

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -10,7 +10,10 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if ( smoothing == 0 )
+        {
+            smoothing = (float)0.1;
+        }
     }
 
     void LateUpdate()
--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    private CharacterController _character;
+    public float moveSpeed;
+    public float jumpSpeed;
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        _character = GetComponent<CharacterController>(); 
+        _rigidbody = GetComponent<Rigidbody>(); 
+        _animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 dir = new Vector3(horizontal,0,vertical);
-
-        if (dir != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(dir);
-            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
-        }
+        Flip();
+        Move();
         
     }
+
+    void Flip()
+    {
+        bool  playerHasXAxisSpeed = Mathf.Abs( _rigidbody.velocity.x ) > Mathf.Epsilon; 
+        if ( playerHasXAxisSpeed )
+        {
+            if( _rigidbody.velocity.x > 0.1f )
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            if( _rigidbody.velocity.x < -0.1f )
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);  //人物翻转180度
+            }
+        }
+    }
+
+    void Move()
+    {
+        float moveDir = Input.GetAxis("Horizontal");
+        Vector3 playerVel = new Vector3( moveDir * moveSpeed , _rigidbody.velocity.y ,0 );
+        _rigidbody.velocity = playerVel;
+
+        bool  playerHasXAxisSpeed = Mathf.Abs( playerVel.x ) > Mathf.Epsilon; // Epsilon为大于0的无限小的数
+        _animator.SetBool("isRun", playerHasXAxisSpeed);
+    }
+
 }
